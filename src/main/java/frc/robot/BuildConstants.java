f5979aef6c1c608d5823d4fdda0879a3c4ddbd32
Willie Pai <PaisWillie@gmail.com>
2026-04-26 13:21:03 -0400

    HTFX: update build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt-2026-Advantage-Kit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-04-24 18:37:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1777070223205L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "d4b9121b2ba95297b29bf60a796a5d3847ec1c09";
+  public static final String GIT_DATE = "2026-04-24 18:39:52 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-04-26 13:20:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1777224026633L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
