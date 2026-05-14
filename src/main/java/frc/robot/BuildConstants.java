3db8836c7a1976c8a3d2b82541ae01370cb67690
Willie Pai <PaisWillie@gmail.com>
2026-05-14 16:32:05 +0200

    HTFX: update swerve encoder to use different id

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt-2026-Advantage-Kit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "5e998778009445c04966c933bfc81978fc07692c";
-  public static final String GIT_DATE = "2026-05-03 02:06:41 EDT";
+  public static final int GIT_REVISION = 24;
+  public static final String GIT_SHA = "aa5c2eab826c78e0da43bcca449452ba69343fc1";
+  public static final String GIT_DATE = "2026-05-03 04:15:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-05-03 04:15:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1777796113805L;
+  public static final String BUILD_DATE = "2026-05-13 10:53:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1778683998099L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
