5587eabc39d4596021a70b7cbe54500d9e397eab
Willie Pai <PaisWillie@gmail.com>
2026-05-26 14:35:27 -0400

    HTFX: linting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt-2026-Advantage-Kit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "3db8836c7a1976c8a3d2b82541ae01370cb67690";
-  public static final String GIT_DATE = "2026-05-14 10:32:05 EDT";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "204c67f61d8b70715eeb14e7589207901fe4ef80";
+  public static final String GIT_DATE = "2026-05-20 12:52:36 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-05-20 12:50:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1779295848491L;
+  public static final String BUILD_DATE = "2026-05-26 14:35:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1779820515106L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,25 +24,26 @@ public class RobotContainer {
   private final IndexerSubsystem indexer;
 
   final CommandPS4Controller driverController = new CommandPS4Controller(0);
-  private final SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final SwerveSubsystem swerve =
+      new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
   // Establish a Sendable Chooser that will be able to be sent to the
   // SmartDashboard, allowing selection of desired auto
   private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
   /**
-   * Converts driver input into a field-relative ChassisSpeeds that is controlled
-   * by angular
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular
    * velocity.
    */
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
-      swerve.getSwerveDrive(),
-      () -> driverController.getLeftY() * -1,
-      () -> driverController.getLeftX() * -1)
-      .withControllerRotationAxis(() -> driverController.getRightX() * -1)
-      .deadband(OperatorConstants.DEADBAND)
-      .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
+  SwerveInputStream driveAngularVelocity =
+      SwerveInputStream.of(
+              swerve.getSwerveDrive(),
+              () -> driverController.getLeftY() * -1,
+              () -> driverController.getLeftX() * -1)
+          .withControllerRotationAxis(() -> driverController.getRightX() * -1)
+          .deadband(OperatorConstants.DEADBAND)
+          .scaleTranslation(0.8)
+          .allianceRelativeControl(true);
 
   public RobotContainer() {
     hood = new HoodSubsystem();
@@ -57,7 +58,7 @@ public class RobotContainer {
 
   public void publishComponentPoses() {
     Logger.recordOutput(
-        "3D/ComponentPoses", new Pose3d[] { linearIntake.getPose3d(), hood.getPose3d() });
+        "3D/ComponentPoses", new Pose3d[] {linearIntake.getPose3d(), hood.getPose3d()});
   }
 
   private void configureBindings() {
