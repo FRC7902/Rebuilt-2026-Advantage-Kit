52217815411d701265f55879a414182156cbca1d
Willie Pai <PaisWillie@gmail.com>
2026-07-16 14:14:32 -0400

    HTFX: fix kG hood feedforward

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt-2026-Advantage-Kit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "204c67f61d8b70715eeb14e7589207901fe4ef80";
-  public static final String GIT_DATE = "2026-05-20 12:52:36 EDT";
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "5587eabc39d4596021a70b7cbe54500d9e397eab";
+  public static final String GIT_DATE = "2026-05-26 14:35:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-05-26 14:35:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1779820515106L;
+  public static final String BUILD_DATE = "2026-07-16 14:14:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1784225645632L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
