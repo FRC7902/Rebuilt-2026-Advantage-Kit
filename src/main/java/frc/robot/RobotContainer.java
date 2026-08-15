08e827c5e43eee7508fd9262f2dbec0d29a0ea6f
aazhang-hub <aazhang@markhamfirebirds.ca>
2026-08-15 18:56:22 -0400

    a4 homework submission

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.RPM;
+
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -13,11 +17,6 @@ import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.LinearIntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
-
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.RPM;
-
 import java.io.File;
 import org.littletonrobotics.junction.Logger;
 import swervelib.SwerveInputStream;
@@ -30,25 +29,26 @@ public class RobotContainer {
   private final IndexerSubsystem indexer;
 
   final CommandPS4Controller driverController = new CommandPS4Controller(0);
-  private final SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final SwerveSubsystem swerve =
+      new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
   // Establish a Sendable Chooser that will be able to be sent to the
   // SmartDashboard, allowing selection of desired auto
   private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
   /**
-   * Converts driver input into a field-relative ChassisSpeeds that is controlled
-   * by angular
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular
    * velocity.
    */
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
-      swerve.getSwerveDrive(),
-      () -> driverController.getLeftY() * -1,
-      () -> driverController.getLeftX() * -1)
-      .withControllerRotationAxis(() -> driverController.getRightX() * -1)
-      .deadband(OperatorConstants.DEADBAND)
-      .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
+  SwerveInputStream driveAngularVelocity =
+      SwerveInputStream.of(
+              swerve.getSwerveDrive(),
+              () -> driverController.getLeftY() * -1,
+              () -> driverController.getLeftX() * -1)
+          .withControllerRotationAxis(() -> driverController.getRightX() * -1)
+          .deadband(OperatorConstants.DEADBAND)
+          .scaleTranslation(0.8)
+          .allianceRelativeControl(true);
 
   public RobotContainer() {
     hood = new HoodSubsystem();
@@ -63,7 +63,7 @@ public class RobotContainer {
 
   public void publishComponentPoses() {
     Logger.recordOutput(
-        "3D/ComponentPoses", new Pose3d[] { linearIntake.getPose3d(), hood.getPose3d() });
+        "3D/ComponentPoses", new Pose3d[] {linearIntake.getPose3d(), hood.getPose3d()});
   }
 
   private void configureBindings() {
@@ -71,7 +71,27 @@ public class RobotContainer {
     swerve.setDefaultCommand(driveFieldOrientedAnglularVelocity);
 
     // Bind commands to buttons on the driver controller here:
+    Command intakingFuel =
+        Commands.sequence(
+            Commands.parallel(
+                linearIntake.setHeightAndStop(Meters.of(0.1)),
+                indexer.set(1),
+                Commands.waitSeconds(5)),
+            linearIntake.setHeightAndStop(Meters.of(0.3132)),
+            indexer.set(0));
+    driverController.button(1).onTrue(intakingFuel);
 
+   Command shootingthefuel = Commands.parallel(
+      shooter.setVelocity(RPM.of(2500)),
+      hood.setAngleAndStop(Degrees.of(25)).andThen(indexer.set(1))
+    );
+    Command notshootingthefuel = Commands.parallel(
+      shooter.setVelocity(RPM.of(0)),
+      hood.setAngleAndStop(Degrees.of(0)),
+      indexer.set(0)
+    );
+    driverController.button(2).whileTrue(shootingthefuel)
+      .onFalse(notshootingthefuel);
   }
 
   /**
